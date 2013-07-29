74cf8bdb668f6cff5a24c7f1f37044eaa5e10e52
kenvilar <kenvilar@gmail.com>
2013-07-29 15:59:37 +0800

    Added addEmployee() function to Moderator controller

--- chat_messenger/grails-app/controllers/chat_messenger/ModeratorController.groovy
+++ chat_messenger/grails-app/controllers/chat_messenger/ModeratorController.groovy
@@ -2,5 +2,31 @@ package chat_messenger
 
 class ModeratorController {
 
-    def index() { }
+    def index() { 
+		render(view:"moderatorPage")
+	}
+
+	def addEmployee() {
+		def db = new Sql(dataSource)
+
+		def firstName = params.firstName
+		def lastName = params.lastName
+		def location = params.location
+		def userName = params.userName
+
+		Date now = new Date()
+		def date = g.formatDate(format:"yyyy", date:new Date())
+		[date:date]
+
+		Random random = new Random()
+		String idCode = (String) random.nextInt(9000) + 1000
+
+		String idNumber = date+"-"+idCode
+
+		db.execute("""insert into listOfEmployee(id,firstName,lastName,location,userName) 
+					values('${idNumber}','${firstName}','${lastName}','${location}','${userName}')""")
+
+		index()
+	}
+
 }
